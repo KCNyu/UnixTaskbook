e65b3e5050c7fceb152b4c8ca2447507be8a23d9
KCNyu <527580487@qq.com>
2021-10-12 16:00:16 +0800

    update

---
+++ inc/color.h
@@ -0,0 +1,13 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define RESET "\033[0m"
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define BROWN "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define MAGENTA "\033[1;35m"
+#define CYAN "\033[1;36m"
+#define WHITE "\033[1;37m"
+
+#endif /* COLOR_H */
