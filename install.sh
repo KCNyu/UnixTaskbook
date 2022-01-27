d8db9c7b6cfa64cd7effc05a73c3508839584c80
KCNyu <527580487@qq.com>
2022-01-27 20:58:30 +0800

    FIX: add shared/obj

--- install.sh
+++ install.sh
@@ -32,20 +32,24 @@ function printWorkSpace() {
 	blue "WORKSPACE: ${WORKSPACE}"
 	echo $FLINES
 }
+
 if [ -d lib ]; then
 	cd lib
 else
 	red "lib not exist!"
 	exit 1
 fi
+
 printWorkSpace
 yellow "Dynamic library file is being compiled"
 
 if ! [ -d obj ]; then mkdir obj; fi
+if ! [ -d ../shared/obj ]; then mkdir ../shared/obj; fi
 if ! make; then
 	red "make failed!"
 	exit 1
 fi
+
 if [ "$(uname)" == "Linux" ]; then
 
 	if ! [ -d /usr/local/lib/TaskChecker ]; then mkdir /usr/local/lib/TaskChecker; fi
@@ -67,8 +71,8 @@ green "Dynamic library installation and configuration completed"
 
 cd ..
 printWorkSpace
-
 yellow "Compile the project kernel"
+
 if ! [ -d obj ]; then mkdir obj; fi
 if ! make; then
 	red "make failed!"
