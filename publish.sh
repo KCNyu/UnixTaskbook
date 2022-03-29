01ba4a59a4d91f5df12448a80de2ab9995dae567
KCNyu <shengyu.li.evgeny@gmail.com>
2022-03-29 22:10:37 +0800

    UPDATE: add publish workflow

--- publish.sh
+++ publish.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-sudo ./recomplie.sh
-
 struct=$(uname -m)
 platform=$(uname)
 
 packagename="UnixTaskbook-$platform-$struct"
 
-cd ..
-rm -rf "$packagename"
 mkdir "$packagename"
 
-cd -
 # unixTaskbook配置文件
-cp ./unixTaskbook ../"$packagename" 
-cp ./lib/*.so ../"$packagename"
-cp ./lib/unixTaskbook.conf ../"$packagename"
+cp ./build/bin/unixTaskbook ./"$packagename"
+if [ "$platform" == "Darwin" ]; then
+    cp ./build/lib/*.dylib ./"$packagename"
+fi
+if [ "$platform" == "Linux" ]; then
+    cp ./build/lib/*.so ./"$packagename"
+    cp ./lib/unixTaskbook.conf ./"$packagename"
+fi
 
 # vscode任务配置文件
-cp ./.vscode/launch.json ../"$packagename"
-cp ./.vscode/tasks.json ../"$packagename" 
+cp ./.vscode/launch.json ./"$packagename"
+cp ./.vscode/tasks.json ./"$packagename" 
 
-cd ..
 tar -zcvf "$packagename".tar.gz "$packagename"
 
 rm -rf "$packagename"
