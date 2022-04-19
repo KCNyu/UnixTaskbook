86bfad56ef48f491688be6383bbe5c28ce8a7167
KCNyu <shengyu.li.evgeny@gmail.com>
2022-04-20 01:48:22 +0800

    UPDATE: rewrite parameter

--- inc/unixTaskbook.hpp
+++ inc/unixTaskbook.hpp
@@ -12,11 +12,13 @@ private:
 	TaskLib *tasklib;
 	std::vector<std::string> supported_compiler = {"gcc", "g++"};
 	int task_argc;
-	std::string tasklib_name;
+
+	std::string task_lib_name;
 	std::string task_name;
+	std::string task_program_path;
 	int task_num;
+
 	std::string language_option;
-	std::string program;
 	std::string compile_out;
 	std::string compile_log;
 	std::string check_dir;
@@ -28,7 +30,7 @@ private:
 protected:
 	void check_task_lib();
 	void parse_command(int argc, char *argv[]);
-	void parse_task_name();
+	void parse_task_name(std::string &program);
 	void parse_compile_argv(std::string program, char **&compile_argv);
 	void parse_execute_argv(char **&execute_argv);
 
@@ -37,14 +39,14 @@ public:
 	void load_task_lib();
 	void close_task_lib();
 	void print_task_info(int task_num, std::string language_option);
-	void compile_program(std::string program);
-	void create_test(std::string program);
-	void execute_program(std::string program);
-	void check_program_result(std::string program, int test_num, bool print_option);
-	void upload_program(std::string program);
-	void check_program_dir(std::string dir);
-	void execute_run(std::string program);
-	void parse_task_name(std::string program);
+	void compile_program(const std::string &program);
+	void create_test();
+	void execute_program();
+	void check_program_result(int test_num, bool print_option);
+	void upload_program(const std::string &program);
+	void check_program_dir(const std::string &dir);
+	void execute_run(const std::string pre_task_name);
+	void parse_task_name(const std::string pre_task_name);
 	void run();
 	~UnixTaskbook();
 };
--- src/unixTaskbook.cpp
+++ src/unixTaskbook.cpp
@@ -12,7 +12,7 @@ UnixTaskbook::~UnixTaskbook()
 void UnixTaskbook::load_task_lib()
 {
 	// load the task library
-	handle = dlopen(tasklib_name.c_str(), RTLD_LAZY);
+	handle = dlopen(task_lib_name.c_str(), RTLD_LAZY);
 	dlopen_error_handle(handle, "Cannot load library: ");
 
 	// load the create symbols
@@ -44,7 +44,7 @@ void UnixTaskbook::check_task_lib()
 	assert(tasklib->get_task_count() == tasklib->task_text_chinese.size());
 
 	// check tasklib name
-	assert(tasklib->library_name == tasklib_name);
+	assert(tasklib->library_name == task_lib_name);
 
 	// check compiler
 	assert(std::find(supported_compiler.begin(), supported_compiler.end(), tasklib->compiler) != supported_compiler.end());
@@ -53,7 +53,7 @@ void UnixTaskbook::print_task_info(int task_num, std::string language_option)
 {
 	if (task_num > tasklib->get_task_count() || task_num <= 0)
 	{
-		LOG_ERROR("Exceeds the number of tasks!\nThe maximum number of %s is %d", tasklib_name.substr(0, tasklib->library_name.size() - 3).c_str(), tasklib->get_task_count());
+		LOG_ERROR("Exceeds the number of tasks!\nThe maximum number of %s is %d", task_lib_name.substr(0, tasklib->library_name.size() - 3).c_str(), tasklib->get_task_count());
 	}
 	if (print_option)
 	{
@@ -91,27 +91,46 @@ void UnixTaskbook::print_task_info(int task_num, std::string language_option)
 	LOG_MESSAGE("%s", utilities::divider.c_str());
 }
 
-void UnixTaskbook::parse_task_name(std::string program)
+void UnixTaskbook::parse_task_name(const std::string pre_task_name)
 {
-	if (program.size() != 0)
+	if (pre_task_name.find("/") != std::string::npos && pre_task_name.find(".c") != std::string::npos)
 	{
 		// get task name without path and extension
-		task_name = program.substr(program.find_last_of("/") + 1, program.find_last_of(".") - program.find_last_of("/") - 1);
+		this->task_name = pre_task_name.substr(pre_task_name.find_last_of("/") + 1, pre_task_name.find_last_of(".") - pre_task_name.find_last_of("/") - 1);
+		this->task_program_path = pre_task_name;
 	}
+	else if (pre_task_name.find("/") == std::string::npos && task_name.find(".c") != std::string::npos)
+	{
+		// get task name without extension
+		this->task_name = pre_task_name.substr(0, pre_task_name.find_last_of("."));
+		this->task_program_path = "./" + pre_task_name;
+	}
+	else if (pre_task_name.find("/") != std::string::npos && task_name.find(".c") == std::string::npos)
+	{
+		// get task name without path
+		this->task_name = pre_task_name.substr(pre_task_name.find_last_of("/") + 1);
+		this->task_program_path = pre_task_name + ".c";
+	}
+	else
+	{
+		this->task_name = pre_task_name;
+		this->task_program_path = "./" + pre_task_name + ".c";
+	}
+
 	std::string numerics("0123456789");
-	std::string::size_type pos = task_name.find_first_of(numerics);
+	std::string::size_type pos = this->task_name.find_first_of(numerics);
 #if defined __linux__
-	tasklib_name = std::string("libutb") + task_name.substr(0, pos) + ".so";
+	task_lib_name = std::string("libutb") + this->task_name.substr(0, pos) + ".so";
 #elif defined __APPLE__
-	tasklib_name = std::string("libutb") + task_name.substr(0, pos) + ".dylib";
+	task_lib_name = std::string("libutb") + this->task_name.substr(0, pos) + ".dylib";
 #endif
-	task_num = atoi(task_name.substr(pos).c_str());
+	task_num = atoi(this->task_name.substr(pos).c_str());
 }
 void UnixTaskbook::parse_command(int argc, char *argv[])
 {
-	command_parser.add<std::string>("taskname", 't', "tasks to view/check", false, "");
+	command_parser.add<std::string>("task", 't', "tasks to view/check", false, "");
 	command_parser.add<std::string>("language", 'l', "language to describe the task info", false, "ru");
-	command_parser.add<std::string>("program", 'p', "checked program(TaskGroupNumber.c), when specifying this parameter, you can not specify the -t parameter", false, "");
+	// command_parser.add<std::string>("program", 'p', "checked program(TaskGroupNumber.c), when specifying this parameter, you can not specify the -t parameter", false, "");
 	command_parser.add<std::string>("directory", 'd', "check all programs in the directory, -p -t parameters are invalid after using this parameter", false, "");
 	command_parser.add("showtest", 's', "show all test when checking");
 	command_parser.add("help", 'h', "display this help and exit");
@@ -125,9 +144,9 @@ void UnixTaskbook::parse_command(int argc, char *argv[])
 	}
 
 	command_parser.parse_check(argc, argv);
-	task_name = command_parser.get<std::string>("taskname");
+	task_name = command_parser.get<std::string>("task");
 	language_option = command_parser.get<std::string>("language");
-	program = command_parser.get<std::string>("program");
+	// program = command_parser.get<std::string>("program");
 	check_dir = command_parser.get<std::string>("directory");
 	print_option = command_parser.exist("showtest");
 
@@ -172,7 +191,7 @@ void UnixTaskbook::parse_compile_argv(std::string program, char **&compile_argv)
 
 	compile_argv[compile_argc + 1] = NULL;
 }
-void UnixTaskbook::compile_program(std::string program)
+void UnixTaskbook::compile_program(const std::string &program)
 {
 	if (!utilities::exists_file(program))
 	{
@@ -180,17 +199,22 @@ void UnixTaskbook::compile_program(std::string program)
 		exit(1);
 	}
 
-	// return if the program is already compiled
-	if (program.find(".out") != std::string::npos)
-	{
-		compile_out = program;
-		return;
-	}
-
 	compile_log = std::string(program.substr(0, strrchr(program.c_str(), '.') - program.c_str()));
 	compile_out = compile_log;
 	compile_log += "." + tasklib->compiler + "log";
 	compile_out += ".out";
+
+	// compare the changetime with the last compile time
+	if (utilities::exists_file(compile_out))
+	{
+		struct stat file_stat, out_stat;
+		stat(program.c_str(), &file_stat);
+		stat(compile_out.c_str(), &out_stat);
+		if (file_stat.st_mtime <= out_stat.st_mtime)
+		{
+			return;
+		}
+	}
 
 	int file_log = open(compile_log.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (!file_log)
@@ -295,7 +319,7 @@ void UnixTaskbook::compile_program(std::string program)
 		}
 	}
 }
-void UnixTaskbook::create_test(std::string program)
+void UnixTaskbook::create_test()
 {
 	tasklib->utb_generate_task_test(task_num);
 	tasklib->utb_generate_task_control(task_num);
@@ -315,7 +339,7 @@ void UnixTaskbook::parse_execute_argv(char **&execute_argv)
 	}
 	execute_argv[execute_argc + 1] = NULL;
 }
-void UnixTaskbook::execute_program(std::string program)
+void UnixTaskbook::execute_program()
 {
 	// std::cout << "Program output:\n"
 	//		  << "-------------------------------------------" << std::endl;
@@ -345,7 +369,7 @@ void UnixTaskbook::execute_program(std::string program)
 		LOG_ERROR("Error during running: %s", compile_out.c_str());
 	}
 }
-void UnixTaskbook::check_program_result(std::string program, int test_num, bool print_option)
+void UnixTaskbook::check_program_result(int test_num, bool print_option)
 {
 	/*
 	LOG_PROCESS(" ------------------");
@@ -390,7 +414,7 @@ void UnixTaskbook::check_program_result(std::string program, int test_num, bool 
 		exit(EXIT_FAILURE);
 	}
 }
-void UnixTaskbook::upload_program(std::string program)
+void UnixTaskbook::upload_program(const std::string &program)
 {
 	if (print_option)
 	{
@@ -410,7 +434,7 @@ void UnixTaskbook::upload_program(std::string program)
 	}
 }
 // check all program files in the directory
-void UnixTaskbook::check_program_dir(std::string dir)
+void UnixTaskbook::check_program_dir(const std::string &dir)
 {
 	if (print_option)
 	{
@@ -426,25 +450,36 @@ void UnixTaskbook::check_program_dir(std::string dir)
 	utilities::get_files_in_dir(dir, files);
 
 	// check all files by using execute_run
+	int count = 0;
 	for (size_t i = 0; i < files.size(); i++)
 	{
 		if (files[i].find(".c") == std::string::npos)
 		{
 			continue;
 		}
+		std::string numerics("0123456789");
+		std::string::size_type pos = task_name.find_first_of(numerics);
+
+		if (command_parser.exist("task") && files[i].find(task_name.substr(0, pos)) == std::string::npos && task_name != "*")
+		{
+			// std::cout << "Skipping file: " << files[i] << std::endl;
+			// std::cout << "It is not a program for task: " << task_name.substr(0, pos) << std::endl;
+			continue;
+		}
 		std::string program = dir + "/" + files[i];
 		if (print_option)
 		{
 			LOG_PROCESS("\n                           --------------------");
-			LOG_PROCESS(">>>>>>>>>>>>>>>>>>>>>>>>>>| Checking program %ld |>>>>>>>>>>>>>>>>>>>>>>>>>>", i + 1);
+			LOG_PROCESS(">>>>>>>>>>>>>>>>>>>>>>>>>>| Checking program %ld |>>>>>>>>>>>>>>>>>>>>>>>>>>", count + 1);
 			LOG_PROCESS("                           --------------------\n");
 		}
 		else
 		{
-			LOG_PROCESS(">>>>>>>>>>>>>>>>>>>>>>>>>>| Checking program %ld |>>>>>>>>>>>>>>>>>>>>>>>>>>", i + 1);
+			LOG_PROCESS(">>>>>>>>>>>>>>>>>>>>>>>>>>| Checking program %ld |>>>>>>>>>>>>>>>>>>>>>>>>>>", count + 1);
 		}
 
 		execute_run(program);
+		count++;
 	}
 
 	if (print_option)
@@ -458,11 +493,11 @@ void UnixTaskbook::check_program_dir(std::string dir)
 		LOG_SUCCESS("<<<<<<<<<<<<<<<<<<<<<| Finish checking the directory |<<<<<<<<<<<<<<<<<<<<<");
 	}
 }
-void UnixTaskbook::execute_run(std::string program)
+void UnixTaskbook::execute_run(const std::string pre_task_name)
 {
 	srand(time(nullptr));
 
-	parse_task_name(program);
+	parse_task_name(pre_task_name);
 
 	load_task_lib();
 
@@ -473,7 +508,7 @@ void UnixTaskbook::execute_run(std::string program)
 			LOG_ERROR("Need to be checked program name or directory");
 		}
 	*/
-	compile_program(program);
+	compile_program(task_program_path);
 
 	for (int i = 0; i < tasklib->total_test_count; i++)
 	{
@@ -488,9 +523,9 @@ void UnixTaskbook::execute_run(std::string program)
 			LOG_PROCESS(">>>>>>>>>>>>>>>>>>>>>>>>>>>>| Running test %d |>>>>>>>>>>>>>>>>>>>>>>>>>>>>", i + 1);
 		}
 
-		create_test(program);
-		execute_program(program);
-		check_program_result(program, i, print_option);
+		create_test();
+		execute_program();
+		check_program_result(i, print_option);
 
 		// remove test files and control file
 		system("rm *.tst");
@@ -517,7 +552,7 @@ void UnixTaskbook::execute_run(std::string program)
 		transform(utb_service.begin(), utb_service.end(), utb_service.begin(), ::tolower);
 		if (utb_service == "true")
 		{
-			upload_program(program);
+			upload_program(task_program_path);
 		}
 	}
 }
@@ -529,6 +564,6 @@ void UnixTaskbook::run()
 	}
 	else
 	{
-		execute_run(program);
+		execute_run(task_name);
 	}
 }
